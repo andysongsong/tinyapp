19002e0d249b23a9425b60449a68d5a3658aa9c4
andysongsong <sxj930916@gmail.com>
2021-08-03 04:19:33 +0000

    add randomString function

--- express_server.js
+++ express_server.js
@@ -39,3 +39,13 @@ app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
+function generateRandomString(length) {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
