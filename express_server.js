7a03dc25c2e9b7f948c57d71491cca24299f8135
andysongsong <sxj930916@gmail.com>
2021-08-03 05:04:47 +0000

    implement redirect after submission

--- express_server.js
+++ express_server.js
@@ -37,7 +37,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(urlDatabase[shortURL]);
+});
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]; // const longURL = ...
+  res.redirect(longURL);
 });
 function generateRandomString(length) {
   let result = "";
