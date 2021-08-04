8e2bfbcf61627a3c7bd1f34efba90b3166c4360d
andysongsong <sxj930916@gmail.com>
2021-08-04 04:40:16 +0000

    add login and logout function

--- express_server.js
+++ express_server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 const generateRandomString = require("./helper");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -23,11 +25,12 @@ const urlDatabase = {
 
 //all get routes.
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
   res.render("urls_new");
 });
 
@@ -35,6 +38,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -62,6 +66,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   let newURL = req.body.newURL;
   urlDatabase[req.params.shortURL] = newURL;
+  res.redirect("/urls");
+});
+
+//route fot login & logout
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
